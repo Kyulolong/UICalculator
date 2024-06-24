c28b8b50863e5bba156ec8cbc593e82f590028fa
kyurorong <zzikcomputer@gmail.com>
2024-06-24 12:27:27 +0900

    level 8까지 완성

--- UICalculator/ViewController.swift
+++ UICalculator/ViewController.swift
@@ -3,41 +3,42 @@
 //  UICalculator
 //
 //  Created by 김인규 on 6/20/24.
-//
+
 
 import UIKit
 import SnapKit
 
-//level 4~6
+//level 4~8
 class ViewController: UIViewController {
-
+    
     let label = UILabel()
-    private var number: Int = 0
+    private var number: String = ""
+    private var result: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
-
+    
     private func configureUI() {
         view.backgroundColor = .black
-
-        let buttondiv = makeButton(titleValue: "/", action: #selector(abc), backgroundColor: UIColor.orange)
-        let buttonequal = makeButton(titleValue: "=", action: #selector(abc), backgroundColor: UIColor.orange)
-        let buttonmul = makeButton(titleValue: "*", action: #selector(abc), backgroundColor: UIColor.orange)
-        let buttonAC = makeButton(titleValue: "AC", action: #selector(abc), backgroundColor: UIColor.orange)
-        let buttonmin = makeButton(titleValue: "-", action: #selector(abc), backgroundColor: UIColor.orange)
-        let buttonadd = makeButton(titleValue: "+", action: #selector(abc), backgroundColor: UIColor.orange)
-        let button1 = makeButton(titleValue: "1", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button2 = makeButton(titleValue: "2", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button3 = makeButton(titleValue: "3", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button4 = makeButton(titleValue: "4", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button5 = makeButton(titleValue: "5", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button6 = makeButton(titleValue: "6", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button7 = makeButton(titleValue: "7", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button8 = makeButton(titleValue: "8", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button9 = makeButton(titleValue: "9", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
-        let button0 = makeButton(titleValue: "0", action: #selector(abc), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        
+        let buttondiv = makeButton(titleValue: "/", action: #selector(divTapped), backgroundColor: UIColor.orange)
+        let buttonequal = makeButton(titleValue: "=", action: #selector(equalTapped), backgroundColor: UIColor.orange)
+        let buttonmul = makeButton(titleValue: "*", action: #selector(mulTapped), backgroundColor: UIColor.orange)
+        let buttonAC = makeButton(titleValue: "AC", action: #selector(resetButton), backgroundColor: UIColor.orange)
+        let buttonmin = makeButton(titleValue: "-", action: #selector(minTapped), backgroundColor: UIColor.orange)
+        let buttonadd = makeButton(titleValue: "+", action: #selector(addTapped), backgroundColor: UIColor.orange)
+        let button1 = makeButton(titleValue: "1", action: #selector(number1Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button2 = makeButton(titleValue: "2", action: #selector(number2Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button3 = makeButton(titleValue: "3", action: #selector(number3Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button4 = makeButton(titleValue: "4", action: #selector(number4Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button5 = makeButton(titleValue: "5", action: #selector(number5Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button6 = makeButton(titleValue: "6", action: #selector(number6Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button7 = makeButton(titleValue: "7", action: #selector(number7Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button8 = makeButton(titleValue: "8", action: #selector(number8Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button9 = makeButton(titleValue: "9", action: #selector(number9Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
+        let button0 = makeButton(titleValue: "0", action: #selector(number0Tapped), backgroundColor: UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0))
         
         let horizontalStackView = makeHorizontalStackView([button7, button8, button9, buttonadd])
         let horizontalStackView2 = makeHorizontalStackView([button4, button5, button6, buttonmin])
@@ -45,13 +46,11 @@ class ViewController: UIViewController {
         let horizontalStackView4 = makeHorizontalStackView([buttonAC, button0, buttonequal, buttondiv])
         let verticalStackView = makeVerticalStackView([horizontalStackView, horizontalStackView2, horizontalStackView3, horizontalStackView4])
         
-        label.text = "\(number)"
+        label.text = "\(result)"
         label.textColor = .white
         label.textAlignment = .right
         label.font = UIFont.boldSystemFont(ofSize: 60)
-        
-        
-    [label, verticalStackView]
+        [label, verticalStackView]
             .forEach{ view.addSubview($0) }
         
         label.snp.makeConstraints {
@@ -108,25 +107,112 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 40
         return button
     }
+    
     @objc
-    private func abc() {
-        print("테스트 중")
+    private func abc(){
+        print("버튼")
+    }
+    @objc
+    private func number1Tapped() {
+        number += "1"
+        label.text = number
+    }
+    @objc
+    private func number2Tapped() {
+        number += "2"
+        label.text = number
+    }
+    @objc
+    private func number3Tapped() {
+        number += "3"
+        label.text = number
+    }
+    @objc
+    private func number4Tapped() {
+        number += "4"
+        label.text = number
+    }
+    @objc
+    private func number5Tapped() {
+        number += "5"
+        label.text = number
+    }
+    @objc
+    private func number6Tapped() {
+        number += "6"
+        label.text = number
+    }
+    @objc
+    private func number7Tapped() {
+        number += "7"
+        label.text = number
+    }
+    @objc
+    private func number8Tapped() {
+        number += "8"
+        label.text = number
+    }
+    @objc
+    private func number9Tapped() {
+        number += "9"
+        label.text = number
+    }
+    @objc
+    private func number0Tapped() {
+        if number != "0" && !number.isEmpty {
+            number += "0"
+            label.text = number
+        } else {
+            return resetButton()
+        }
+    }
+    
+    @objc
+    private func addTapped() {
+        number += "+"
+        label.text = number
+    }
+    @objc
+    private func minTapped() {
+        number += "-"
+        label.text = number
+    }
+    @objc
+    private func mulTapped() {
+        number += "*"
+        label.text = number
+    }
+    @objc
+    private func divTapped() {
+        number += "/"
+        label.text = number
+    }
+    @objc
+    private func equalTapped() -> String {
+        if let result = calculate(expression: number) {
+            self.number = String(result)
+            label.text = number
+            return number
+        } else{
+            return ""
+        }
+    }
+    @objc
+    private func resetButton() {
+        self.number = ""
+        label.text = "0"
+    }
+    
+    func calculate(expression: String) -> Int? {
+        let expression = NSExpression(format: expression)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            return result
+        } else {
+            return nil
+        }
     }
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -134,7 +220,7 @@ class ViewController: UIViewController {
 //class ViewController: UIViewController {
 //
 //    let label = UILabel()
-//    
+//
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
 //        configureUI()
@@ -146,7 +232,7 @@ class ViewController: UIViewController {
 //        label.textColor = .white
 //        label.textAlignment = .right
 //        label.font = UIFont.boldSystemFont(ofSize: 60)
-//        
+//
 //    [label]
 //            .forEach{ view.addSubview($0) }
 //
@@ -156,7 +242,7 @@ class ViewController: UIViewController {
 //            $0.top.equalToSuperview().offset(200)
 //            $0.height.equalTo(100)
 //        }
-   
+
 //    }
 
 //}
@@ -175,7 +261,7 @@ class ViewController: UIViewController {
 //    let button5 = UIButton()
 //    let button6 = UIButton()
 //    let buttonmin = UIButton()
-//    
+//
 //    let button1 = UIButton()
 //    let button2 = UIButton()
 //    let button3 = UIButton()
@@ -186,7 +272,7 @@ class ViewController: UIViewController {
 //    let buttonequal = UIButton()
 //    let buttondiv = UIButton()
 //
-//    
+//
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
 //        configureUI()
@@ -198,126 +284,126 @@ class ViewController: UIViewController {
 //        let horizontalStackView2 = makeHorizontalStackView([button4, button5, button6, buttonmin])
 //        let horizontalStackView3 = makeHorizontalStackView([button1, button2, button3, buttonmul])
 //        let horizontalStackView4 = makeHorizontalStackView([buttonAC, button0, buttonequal, buttondiv])
-//        
+//
 //        let verticalStackView = makeVerticalStackView([horizontalStackView, horizontalStackView2, horizontalStackView3, horizontalStackView4])
-//        
+//
 //        label.text = "12345"
 //        label.textColor = .white
 //        label.textAlignment = .right
 //        label.font = UIFont.boldSystemFont(ofSize: 60)
-//        
+//
 //        button7.setTitle("7", for: .normal)
 //        button7.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button7.layer.cornerRadius = 40
 //        button7.frame.size.height = 80
 //        button7.frame.size.width = 80
-//        
+//
 //        button8.setTitle("8", for: .normal)
 //        button8.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button8.layer.cornerRadius = 40
 //        button8.frame.size.height = 80
 //        button8.frame.size.width = 80
-//        
+//
 //        button9.setTitle("9", for: .normal)
 //        button9.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button9.layer.cornerRadius = 40
 //        button9.frame.size.height = 80
 //        button9.frame.size.width = 80
-//        
+//
 //        buttonadd.setTitle("+", for: .normal)
 //        buttonadd.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttonadd.layer.cornerRadius = 40
 //        buttonadd.frame.size.height = 80
 //        buttonadd.frame.size.width = 80
-//        
+//
 //        button4.setTitle("4", for: .normal)
 //        button4.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button4.layer.cornerRadius = 40
 //        button4.frame.size.height = 80
 //        button4.frame.size.width = 80
-//        
+//
 //        button5.setTitle("5", for: .normal)
 //        button5.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button5.layer.cornerRadius = 40
 //        button5.frame.size.height = 80
 //        button5.frame.size.width = 80
-//        
+//
 //        button6.setTitle("6", for: .normal)
 //        button6.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button6.layer.cornerRadius = 40
 //        button6.frame.size.height = 80
 //        button6.frame.size.width = 80
-//        
+//
 //        buttonmin.setTitle("-", for: .normal)
 //        buttonmin.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttonmin.layer.cornerRadius = 40
 //        buttonmin.frame.size.height = 80
 //        buttonmin.frame.size.width = 80
-//        
+//
 //        button1.setTitle("1", for: .normal)
 //        button1.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button1.layer.cornerRadius = 40
 //        button1.frame.size.height = 80
 //        button1.frame.size.width = 80
-//        
+//
 //        button2.setTitle("2", for: .normal)
 //        button2.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button2.layer.cornerRadius = 40
 //        button2.frame.size.height = 80
 //        button2.frame.size.width = 80
-//        
+//
 //        button3.setTitle("3", for: .normal)
 //        button3.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button3.layer.cornerRadius = 40
 //        button3.frame.size.height = 80
 //        button3.frame.size.width = 80
-//        
+//
 //        buttonmul.setTitle("*", for: .normal)
 //        buttonmul.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttonmul.layer.cornerRadius = 40
 //        buttonmul.frame.size.height = 80
 //        buttonmul.frame.size.width = 80
-//        
+//
 //        buttonAC.setTitle("AC", for: .normal)
 //        buttonAC.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttonAC.layer.cornerRadius = 40
 //        buttonAC.frame.size.height = 80
 //        buttonAC.frame.size.width = 80
-//        
+//
 //        button0.setTitle("0", for: .normal)
 //        button0.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        button0.layer.cornerRadius = 40
 //        button0.frame.size.height = 80
 //        button0.frame.size.width = 80
-//        
+//
 //        buttonequal.setTitle("=", for: .normal)
 //        buttonequal.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttonequal.layer.cornerRadius = 40
 //        buttonequal.frame.size.height = 80
 //        buttonequal.frame.size.width = 80
-//        
+//
 //        buttondiv.setTitle("/", for: .normal)
 //        buttondiv.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 ////        buttondiv.layer.cornerRadius = 40
 //        buttondiv.frame.size.height = 80
 //        buttondiv.frame.size.width = 80
-//        
+//
 //    [label, verticalStackView]
 //            .forEach{ view.addSubview($0) }
-//        
+//
 //        label.snp.makeConstraints {
 //            $0.leading.equalToSuperview().offset(30)
 //            $0.trailing.equalToSuperview().offset(-30)
 //            $0.top.equalToSuperview().offset(200)
 //            $0.height.equalTo(100)
 //        }
-//        
+//
 //        horizontalStackView.snp.makeConstraints {
 ////            $0.leading.equalToSuperview().offset(20)
 ////            $0.trailing.equalToSuperview().offset(-20)
 //            $0.height.equalTo(80)
 //        }
-//        
+//
 //        horizontalStackView2.snp.makeConstraints {
 ////            $0.leading.equalToSuperview().offset(20)
 ////            $0.trailing.equalToSuperview().offset(-20)
@@ -339,7 +425,7 @@ class ViewController: UIViewController {
 ////            $0.top.equalTo(horizontalStackView3.snp.bottom).offset(-10)
 //            $0.height.equalTo(80)
 //        }
-//        
+//
 //        verticalStackView.snp.makeConstraints {
 //            $0.width.equalTo(350)
 //            $0.top.equalTo(label.snp.bottom).offset(60)
